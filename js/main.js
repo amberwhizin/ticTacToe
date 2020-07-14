20c367185235cd5e4a82ab8c1bfd11d661a569b9
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-07-14 13:24:59 -0400

    re-did html adn css code, made a 3x3 grid.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// a function that loops through and arr and grabs and x or an o and applys it to the board
+// who starts first - random
+// if its not xs turn its o's turn
+// loop throught a array of 3 x's and 3 o's
+// when user clicks and takes there turn, no one else may use that cell
+// add text when user clicks on any open spot on the screen
