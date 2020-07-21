4ca9b112f30cb60dd57489ad03b38e6d2cff0982
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2020-07-21 13:19:06 -0400

    x and o are now clickable in browser

--- js/main.js
+++ js/main.js
@@ -1,41 +1,73 @@
-let log = document.getElementById('log');
+const cells = document.querySelectorAll(".cell");
 
-document.onclick = inputChange;
+let isX = true;
+const getMarker = () => {
+  const marker = isX ? "X" : "O";
+  isX = !isX;
+  return marker;
+};
 
-function inputChange(arr) {
+// when I click the screen x appears in a Random cell,
+// Unless I chose to be o, make those buttons x, o
+// There is a computer = x and a user = o
+// they have defaults
+// I go first
+// The computer goes next with - event input?
 
-    for (i = 0; i < arr.length; i++) {
-        const currEl = arr[i]
-         log.textContent = `${currEl}, ${currEl})`;
-         console.log(currEl);
+//user turn
+for (let i = 0; i < cells.length; i++) {
+  const currentCell = cells[i];
+  const onClick = (event) => {
+    currentCell.innerHTML = getMarker();
+  };
+  currentCell.addEventListener("click", onClick);
 }
-    }
-inputChange([x, o, x, o, x, o])
+// computer turn
+// do we need an array?
+// const arr = [X, X, X, O, O, O] ???
+// for loop reverse is doing to 
+// for (let j = cells.length; j >= 0; j--) {
+//   const currCompCell = cells[j];
+
+//   const computerTurn = (event) => {
+//     currCompCell.innerHTML = getMarker();
+//   };
+//   currCompCell.addEventListener("input", appear); //input?
+// }
+
+// currentCell.innerHTML = "X";
+
+// let bool = true;
+
+// if (!bool) {
+//   bool = true;
+//   currentCell.innerHTML = "X";
+// } else {
+//   bool = false;
+//   currentCell.innerHTML = "O";
+// }
+
+//   currentCell.addEventListener("change", (event) => {
+//     //console.log(currentCell ? currentCell.innerHTML = "O" : currentCell.innerHTML = "X");
+//     // currentCell.innerHTML = "O";
+//     // const result = document.querySelector(".result");
+//     // result.textContent = `pop${event.target.class}`;
+//     //event.stopPropagation();
+//   });
+// }
+
+// cells[3].addEventListener("click", (event) => {
+//     cells[3].innerHTML = "X";
+// });
+
+// inputChange([x, o, x, o, x, o])
 
 
-// a function that loops through and arr and grabs and x or an o and applys it to the board
-// const xoxo = (arr) => {
-//     // who starts first - random method x or o
-//     let userX = [];
-//     let userO = [];
-//     const letters = [x, o, x, o, x, o];
-//     // if its not xs turn its o's turn
-//     for (i = 0; i < letters.length; i++) {
-//         const currentEl = letters[i];
-
-//         if (currentEl !== 'x') {
-//             userO.push(currentEl);
-//         } else {
-//             userX.push(currentEl);
-//         }
-//     }
-//     if (userX || userO < 3) { //indexOf()?
 //         prompt(`It's ${} turn!`);
 //     } else if (userX || userO === 3 { //indexOf()?
 //         prompt(`${} has Won!`)
 //     }
-    
-    
+
 //     // loop throught a array of 3 x's and 3 o's
 //     // when user clicks and takes there turn, no one else may use that cell
 //     // add text(x or o) when user clicks on any open spot on the screen
@@ -43,7 +75,7 @@ inputChange([x, o, x, o, x, o])
 // }
 
 // who starts first - random
-// if its not xs turn its o's turn
+// if its not x's turn its o's turn
 // loop throught a array of 3 x's and 3 o's
 // when user clicks and takes there turn, no one else may use that cell
-// add text when user clicks on any open spot on the screen
+// add text when user clicks on any open spot on the
